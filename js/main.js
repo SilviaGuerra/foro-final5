e734cd2c57f003a0f860d9ee160e53a87390ba83
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-06-15 12:05:47 -0500

    imprime contenido, autor y numero de respuestas

--- js/main.js
+++ js/main.js
@@ -14,14 +14,25 @@ var cargarTemas = function (){
 }
 
 var crearPublicaciones = function (tema){
+  var contenido = tema.content;
   var autor = tema.author_name;
+  var respuestas = tema.responses_count;
   //creamos fila
   var $fila = $("<tr>");
+  //creamos celda de contenido
+  var $celdaContenido = $("<td>");
+  $celdaContenido.text(contenido);
   // creamos celda de autor
   var $celdaNombre = $("<td>");
   $celdaNombre.text(autor);
+  //creamos celda de respuestas
+  var $celdaRespuestas = $("<td>");
+  $celdaRespuestas.text(respuestas);
+
   // //agregar celda a fila
+  $fila.append($celdaContenido);
   $fila.append($celdaNombre);
+  $fila.append($celdaRespuestas);
 
   // //agregamos fila a tabla e imprimimos
   $("#listaTemas").append($fila);
